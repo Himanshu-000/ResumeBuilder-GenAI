a9631b21cc873707f0073b418bf66e81338971a7
Himanshu Magarde <himanshumagarde512@gmail.com>
2026-07-05 11:47:24 +0530

    first-commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,33 +1,23 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
+const express = require("express")
+const cookieParser = require("cookie-parser")
 const cors = require("cors")
 
+const app = express()
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-const allowedOrigins = process.env.CLIENT_URLS.split(",");
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
-app.use(
-  cors({
-    origin(origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
-  })
-);
+/* require all the routes here */
+const authRouter = require("./routes/auth.routes")
+const interviewRouter = require("./routes/interview.routes")
 
 
-//required all the auth routes
-const auth_routes = require("./Routes/auth.routes");
-const InterViewRouter = require("./Routes/interview.routes")
+/* using all the routes here */
+app.use("/api/auth", authRouter)
+app.use("/api/interview", interviewRouter)
 
-
-//using all the routes here
-app.use("/api/auth",auth_routes);
-app.use("/api/interview",InterViewRouter)
-
-module.exports = app;
+module.exports = app
